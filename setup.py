de8220ee45bc43f83c8c99bd4c86f506a1395155
priyankabawankar-ai <priyankabawankar79@gmail.com>
2026-03-24 13:37:28 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Priyanka Anurag Bawankar",
+    author_email="priyankabawankar79@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+
+)
